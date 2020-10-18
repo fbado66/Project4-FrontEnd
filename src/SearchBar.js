d56838a77eb7ca1c8a490fc5d7230758326a95d0
fbado66 <fbado66@gmail.com>
2020-10-17 23:58:44 -0400

    creating the fron-end

---
+++ src/SearchBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Search = (props) => {
+
+ let handleInput = (evt) => {
+  }
+  return (
+    <div className='header'>
+           
+           
+            <div className='header__center'>
+                <input type="text" />
+                {/* <SearchIcon /> */}
+            </div>
+
+            <div className='header__right'>
+                <p>Become a host</p>
+                {/* <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar /> */}
+            </div>
+        </div>
+  );
+};
+
+export default Search;
