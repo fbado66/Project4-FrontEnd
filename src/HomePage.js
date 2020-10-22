02f5b3d1db419ef762ebd09810a7e301eac8ec89
fbado66 <fbado66@gmail.com>
2020-10-21 20:41:25 -0400

    adding bootstrap framework

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,7 @@
 import React, {Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 // import CategoryList from './CategoryList'
 
 
@@ -34,17 +37,79 @@ class HomePage extends Component {
                 </div>
 
                 <div className = 'filler-home'>
-                    <div className ='filler-header'>Online Experiences: Field Trips</div>
-                    <p className= 'filler-text'>Due to Covid-19 Our team at DuckBnb has paused Field Trips <br/>
-                    We will keep you inform on any upcoming changes</p>
-                    <div className = 'filler-images'>
-                        <div className ='filler-1'>
-                            <img className = 'filler-mainImg' src = 'https://a0.muscache.com/im/pictures/4e1d29d2-5148-444d-945d-c278121675a3.jpg?im_w=1200' />
-                            <p>Decode the science of 2020 with Bill Nye</p>
-                        </div>
-                    </div>
+                    <Row> 
+                        <div className ='filler-header'>Online Experiences: Field Trips</div>
+                    </Row>
 
-                </div>
+                    <Row> 
+                        <p className= 'filler-text'>Due to Covid-19 Our team at DuckBnb has paused Field Trips <br/>
+                        We will keep you inform on any upcoming changes</p>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div className = 'filler-card-avatar'>
+                                <img className = 'avatar-image'src ="https://media.moddb.com/cache/images/groups/1/19/18020/thumb_620x2000/Katara_Sokka_Aang_Momo_Appa.jpg" />
+                                <p className= 'filler-text'>Join the Code Bender in a new journey as they coming close to graduation date</p>
+                            </div>
+                        </Col>
+                        <Col>
+                            <Row>
+                                <Col>
+                                    <div className = 'filler-card'>
+                                        <img className ='filler-image'src = 'https://a0.muscache.com/im/pictures/4e1d29d2-5148-444d-945d-c278121675a3.jpg?im_w=480' />
+                                        <p className= 'filler-text'>Decode the science of 2020 with Bill Nye</p>
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div className = 'filler-card'>
+                                        <img className ='filler-image' src = "https://a0.muscache.com/im/pictures/5d6ee25d-0d8e-472a-b8e7-56bb498b1403.jpg?im_w=480" />
+                                        <p className= 'filler-text'>Discover Buenos Aires street art and sketch</p>
+                                    </div>
+                                </Col>  
+                            </Row>
+                            <Row>
+                                <div className = 'filler-card-wide'>
+                                    <img className ='filler-image' src= "https://a0.muscache.com/im/pictures/c4975e3c-32c2-4bca-9bc7-25a3e69ea341.jpg?im_w=480" />
+                                    <p className = 'filler-text'>Go on a leopard safari in Sri Lanka</p>
+                                </div>
+                            </Row>
+                        </Col>
+                    </Row>
+                   
+                    
+                    {/* <Row>
+                        <Col>
+                            <div className ='filler-header'>Online Experiences: Field Trips</div>
+                            <p className= 'filler-text'>Due to Covid-19 Our team at DuckBnb has paused Field Trips <br/>
+                                We will keep you inform on any upcoming changes
+                            </p>
+                        </Col>
+
+                        <Col>
+                            <img className = 'avatar-image'src ="https://media.moddb.com/cache/images/groups/1/19/18020/thumb_620x2000/Katara_Sokka_Aang_Momo_Appa.jpg" />
+                            <p className= 'filler-text'>Join the Code Bender in a new journey as they coming close to graduation date</p>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col>
+                            <img className ='filler-image'src = 'https://a0.muscache.com/im/pictures/4e1d29d2-5148-444d-945d-c278121675a3.jpg?im_w=480' />
+                            <p className= 'filler-text'>Decode the science of 2020 with Bill Nye</p>
+                        </Col>
+
+                        <Col>
+                        </Col>
+
+                        <Col>
+                            <img className ='filler-image' src= "https://a0.muscache.com/im/pictures/c4975e3c-32c2-4bca-9bc7-25a3e69ea341.jpg?im_w=480" />
+                            <p className = 'filler-text'>Go on a leopard safari in Sri Lanka</p>
+
+
+
+                        </Col>
+                    </Row>
+                 */}
+</div>
                 <div className = 'category-homepage'>
                     
                 </div>
